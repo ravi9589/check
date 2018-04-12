99d0c52d0bc79b0edb6e3f281a73a04f4f70c2b1
ravi9589 <38316802+ravi9589@users.noreply.github.com>
2018-04-12 17:08:45 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+﻿import webbrowser
+import time
+msg="hello world"
+no_breaks=3
+break_count=0
+while (break_count<no_breaks):
+    print(msg)
+    break_count+=1
+    print(no_breaks)
+    time.sleep(10)
+    webbrowser.open_new("https://www.youtube.com/watch?v=OJCkO1w9JoY")
+#for a in range(3):
+
